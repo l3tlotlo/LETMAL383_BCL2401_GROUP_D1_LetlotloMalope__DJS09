52cb9a83aeda21e612431c17968a5000631cae5e
Letlotlo Malope <Software.Engineer47@bafokeng.local>
2024-06-11 00:08:04 +0200

    created 'you' object with poersonal message that also verifies return visit

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
-const reviewTotalDisplay = document.querySelector('#reviews')
+const reviewTotalDisplay: HTMLElement = document.querySelector('#reviews')!
+const returningUserDisplay: HTMLElement = document.querySelector('#returningUser')!
+const userNameDisplay: HTMLElement = document.querySelector('#userName')!
 
 const reviews = [
     {
@@ -21,10 +23,23 @@ const reviews = [
     },
 ]
 
-
-function showReviewTotal (value : number, reviewer: string, isLoyalty : boolean) {
+function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
     const iconDisplay = isLoyalty ? '⭐' : ''
     reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay
 }
 
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
+
+const you = {
+    userName: 'Bobby',
+    isReturning: true,
+}
+
+function populateUser(isReturning: boolean, userName: string) {
+    if (isReturning) {
+        returningUserDisplay.innerHTML = 'back'
+    }
+    userNameDisplay.innerHTML = userName
+}
+
+populateUser(you.isReturning, you.userName)
