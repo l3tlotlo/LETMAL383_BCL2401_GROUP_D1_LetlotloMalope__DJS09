b373df96fd72bd77e26b083de0a0b9c4b4c59fca
Letlotlo Malope <Software.Engineer47@bafokeng.local>
2024-06-11 00:59:36 +0200

    added 'stayedAt key and specified it is an array that accepts strings, specified the details for Bobby Brown

--- index.ts
+++ index.ts
@@ -36,11 +36,13 @@ const you: {
     lastName: string;
     isReturning: boolean;
     age: number;
+    stayedAt: string[];
 } = {
    firstName: 'Bobby', 
    lastName: 'Brown',
    isReturning: true,
-   age: 35
+   age: 35,
+   stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
 }
 
 function populateUser(isReturning: boolean, userName: string) {
@@ -50,4 +52,4 @@ function populateUser(isReturning: boolean, userName: string) {
     userNameDisplay.innerHTML = userName
 }
 
-populateUser(you.isReturning, you.userName)
+populateUser(you.isReturning, you.firstName)
