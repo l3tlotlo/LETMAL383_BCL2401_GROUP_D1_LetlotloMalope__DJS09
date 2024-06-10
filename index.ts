62f930cf298f90ee0c5ed006b6fe9a2d0c0c1d5f
Letlotlo Malope <Software.Engineer47@bafokeng.local>
2024-06-10 23:47:48 +0200

    function to show latest reviewer's name alongside total

--- index.ts
+++ index.ts
@@ -26,8 +26,8 @@ const reviews = [
 ]
 
 //function accepting setting script to only accept numbers
-function showReviewTotal (value : number) {
-    reviewTotalDisplay.innerHTML = 'review total ' + value.toString()
+function showReviewTotal (value : number, reviewer: string) {
+    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer
 }
 
-showReviewTotal(reviews.length)
+showReviewTotal(reviews.length, reviews[0].name)
