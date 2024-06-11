ec65249d0b29639ac96be05001cc3f3b46a96294
Letlotlo Malope <Software.Engineer47@bafokeng.local>
2024-06-11 10:08:47 +0200

    added property information

--- index.ts
+++ index.ts
@@ -50,6 +50,60 @@ const you: {
    stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
 }
 
+const properties : {
+    title: string;
+    image: string;
+    isAvailable: boolean;
+    contact: string;
+    price: number;
+    location: {
+        firstLine: string;
+        city: string;
+        code: number;
+        country: string;
+    };
+}[] = [
+    {
+        image: '',
+        title: "Cottage",
+        price: 68,
+        location: {
+            firstLine: "house 54",
+            city: "Gazalkent",
+            code: 54546,
+            country: "Uzbekistan"
+        },
+        contact: "kinggheedorah@mfdoom.com",
+        isAvailable: false  
+    },
+    {
+        image: '',
+        title: 'Log Cabin',
+        price: 54,
+        location: {
+            firstLine: "no 75",
+            city: "Far Far away",
+            code: 9818,
+            country: 'Ofmilkandhoney'
+        },
+        contact: 'Shrek@hotmail.com',
+        isAvailable: true
+    },
+        {
+        image: '',
+        title: "Igloo",
+        price: 565,
+        location: {
+            firstLine: 'no 01',
+            city: 'North Pole',
+            code: 3903,
+            country: "Antarctica"
+        },
+        contact: "stNick@northpole.com",
+        isAvailable: false 
+    }
+]
+
 function populateUser(isReturning: boolean, userName: string) {
     if (isReturning) {
         returningUserDisplay.innerHTML = 'back'
